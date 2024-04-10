168506c3636f9b9519eb27c9abe97c12f7ca7c97
Muhamedall <mohamedallaoui425@gmail.com>
2024-04-10 12:02:20 +0000

    the last update

--- react/src/components/NavBar/Navbar.jsx
+++ react/src/components/NavBar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 
   return (
     <>
-      <header className=" max-sm:bg-red-300 max-sm:drop-shadow-md  max-sm:static drop-shadow-md  lg:static drop-shadow-md header sticky top-0 lg:bg-slate-50 lg:shadow-md flex items-center justify-between px-8 ">
+      <header className="  max-sm:drop-shadow-md  max-sm:static drop-shadow-md  lg:static drop-shadow-md header sticky top-0 lg:bg-slate-50 lg:shadow-md flex items-center justify-between px-8 ">
           <div className='logo-title hidden sm:block  '>
           <a href="/">
             <img src={logo} alt="logo" className="w-20"></img>
@@ -44,7 +44,7 @@ const Navbar = () => {
        
           <div className="max-sm:absolute  sm:inline-flex max-w-full items-center">
             <button
-              className="max-sm:bg-red-700 max-sm:p-0 max-sm:text-4xl drop-shadow-2xl max-sm:rounded-full max-sm:p-0 max-sm:h-[5%] border-solid border-1 border-green-200 relative flex lg:w-[650%] flex-shrink items-center rounded-full py-2 pl-1  mt-5 h-[50%] lg:border-1 lg:border-green-200 lg:relative lg:flex lg:w-60 lg:flex-shrink lg:flex-grow-0 lg:items-center lg:rounded-full lg:border lg:drop-shadow-2xl lg:border-solid lg:border-2 border-red-200 lg:px-1 lg:py-1  lg:pl-2"
+              className="max-sm:p-[15px] max-sm:w-[230%] max-sm:ml-[15%]  max-sm:bg-red-700 max-sm:p-0 max-sm:text-4xl drop-shadow-2xl max-sm:rounded-full max-sm:p-2 max-sm:h-[5%] border-solid border-1 border-green-200 relative flex lg:w-[650%] flex-shrink items-center rounded-full py-2 pl-1  mt-5 h-[50%] lg:border-1 lg:border-green-200 lg:relative lg:flex lg:w-60 lg:flex-shrink lg:flex-grow-0 lg:items-center lg:rounded-full lg:border lg:drop-shadow-2xl lg:border-solid lg:border-2 border-red-200 lg:px-1 lg:py-1  lg:pl-2"
               type="button"
             >
               <div className="block flex-shrink flex-grow overflow-hidden">Start your search</div>
@@ -52,9 +52,9 @@ const Navbar = () => {
               <div className="relative flex h-8 w-8  items-center justify-center rounded-full"></div>
             </button>
           </div>
-          <ul className="flex ml-[300%] lg:flex lg:items-center lg:ml-[160%]   ">
+          <ul className="absolute max-sm:bg-green-200 max-sm:ml-[230%] max-sm:mt-[5%]  flex ml-[300%] lg:flex lg:items-center lg:ml-[160%]   ">
          
-            <li className="p-4  border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+            <li className=" p-4  border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
               <Link>
                 <FontAwesomeIcon icon={faUser} onClick={handleShow} />
               </Link>
@@ -72,7 +72,7 @@ const Navbar = () => {
         </div>
         {showProfile ? (
         
-        <div className="absolute bg-slate-50 ml-[230%] mt-[75%]      lg:rounded lg:bg-slate-50 lg:w-[20%] lg:py-5 lg:absolute  lg:ml-[75%] lg:mt-[21%]   ">
+        <div className="absolute bg-slate-50  mt-[75%] max-sm:mt-[125%] max-sm:ml-[180%]  max-sm:w-[70%] max-sm:p-5   lg:rounded lg:bg-slate-50 lg:w-[20%] lg:py-5 lg:absolute  lg:ml-[75%] lg:mt-[21%]   ">
           <div className="max-w-sm rounded overflow-hidden ">
             <div className="px-6 py-4">
               <button className="inline-flex items-center justify-center p-1 text-base font-medium text-gray-900 rounded-lg bg-gray-50 hover:text-green-500 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleShowLogine}>
